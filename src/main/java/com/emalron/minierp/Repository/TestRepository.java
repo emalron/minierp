4b976b821d2f52d91339117e4cb60fe3423b4a53
Jes Lee <pjm2108@naver.com>
2025-08-07 12:16:29 +0900

    deleted unused files

--- src/main/java/com/emalron/minierp/Repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.emalron.minierp.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.emalron.minierp.Entity.TestItem;
-
-public interface TestRepository extends JpaRepository<TestItem, Long> {
- 
-}
